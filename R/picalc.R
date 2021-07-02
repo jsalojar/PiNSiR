d75f4e762b8d65db8405bbee298f64b61cbd083a
Jarkko Salojärvi <jarkko@Raakalainen-laptop.local>
2021-07-02 16:06:36 +0300

    added zero-fold and four-fold position identification based on gff

--- R/picalc.R
+++ R/picalc.R
@@ -8,22 +8,85 @@ filter.ninth=function(x,ID="Parent"){
   return(idvec)
 }
 
+folds.0=function(gff){
+  genes=unique(gff[,4])
+  for (i in 1:length(genes)){
+     gff1=gff[gff[,4]==genes[i],]
+     exo=gff1[,3]-gff1[,2]+1
+     if (gff1[1,5]=="-"){
+        posvec=rep(c(3,2,1),length.out=sum(exo))
+     }else{
+        posvec=rep(c(1,2,3),length.out=sum(exo))
+     }
+     coordvec=c()
+     for (j in 1:nrow(gff1))
+        coordvec=c(coordvec,gff1[j,2]:gff1[j,3])
+     zero.fold=coordvec[posvec==1 | posvec==2]
+     df.0fold=data.frame(Chr=gff1[1,1],pos=zero.fold)
+     if (i==1){
+       all.0fold=df.0fold
+     }else{
+       all.0fold=rbind(all.0fold,df.0fold)
+     }
+     message(i)
+  }
+  return(data.frame(zero.fold=all.0fold))
+}     
+
+folds.4=function(gff,peptide.fasta){
+  fourfold=c("V","S","P","T","A","G")
+  #read peptide fasta
+  pep=seqinr::read.fasta(peptide.fasta,seqtype="AA")
+  pep=pep[unique(gff[,4])]
+  ngenes=length(pep)
+  for (i in 1:ngenes){
+     gff1=gff[gff[,4]==names(pep)[i],]
+     exo=gff1[,3]-gff1[,2]+1
+     if (gff1[1,5]=="-"){
+        posvec=rep(c(3,2,1),length.out=sum(exo))
+        protvec=cumsum(posvec==3)
+     }else{
+        posvec=rep(c(1,2,3),length.out=sum(exo))
+        protvec=cumsum(posvec==1)
+     }
+     coordvec=c()
+     for (j in 1:nrow(gff1))
+        coordvec=c(coordvec,gff1[j,2]:gff1[j,3])
+     zero.fold=coordvec[posvec==1 | posvec==2]
+     #Val (V),Ser (S),Pro (P),Thr (T),Ala (A),Gly (G)
+     pepvec=which(pep[[i]] %in% fourfold)
+     four.fold=coordvec[protvec %in% pepvec & posvec==3]
+     df.4dtv=data.frame(Chr=gff1[1,1],pos=four.fold)     
+     if (i==1){
+       all.4dtv=df.4dtv
+     }else{
+       all.4dtv=rbind(all.4dtv,df.4dtv)
+     }
+     message(i)
+  }
+  return(four.fold=all.4dtv)
+}     
+
 #' @export
 filter.hq.genes=function(gff.file,peptide.fasta,bed=F){
 
+  #read in gff
   gff=read.delim(gff.file,comment.char="#",header=F,as.is=T)
-  gff=gff[gff[,3]=="exon",c(1,4,5,9)]
+  gff=gff[gff[,3]=="exon",c(1,4,5,9,7)]
   #Prepare a gff file with exons, 4th column is the parent gene ID.
   gff[,4]=filter.ninth(gff[,4])
   # split according to gene ID
   gff.split=split(gff,gff[,4])
 
-  #Check that the cds is a multiple of 3 (intact codons)
-  gene.OK=which(sapply(gff.split,function(x) sum(x[,3]-x[,2]+1)%%3)==0)
-
+  #read peptide fasta
   pep=seqinr::read.fasta(peptide.fasta,seqtype="AA")
+
   #Check that the protein begins with M
   pep.OK=which(sapply(pep,function(x) x[1]=="M"))
+
+  #Check that the cds is a multiple of 3 (intact codons)
+  gene.OK=which(sapply(gff.split,function(x) sum(x[,3]-x[,2]+1)%%3)==0)
+  #Whole protein is OK - both checks passed
   prot.OK=intersect(names(pep.OK),names(gene.OK))
 
   # In case we want to get a more accurate estimate, look at codons to define N nonsyn positions
@@ -47,11 +110,10 @@ filter.hq.genes=function(gff.file,peptide.fasta,bed=F){
     all[,3]=all[,3]-1
   }
   return(list(HQ=hq,ALL=all))
-
 }
 
 
-contig.Pin=function(TH,exs,ex.mut){
+contig.Pin=function(TH,exs,ex.mut,datatype){
   th.sum=0;Nsnp=nrow(ex.mut)
   accept.vec=vector("logical",nrow(ex.mut))
   for (i in 1:nrow(ex.mut)){
@@ -64,13 +126,17 @@ contig.Pin=function(TH,exs,ex.mut){
    ii=which(TH[,2] %in% ex.mut[accept.vec,2])
    th.sum=sum(exp(TH[ii,4]))
    #For now, assume no missing data
-   N.nonsy=2*sum(exs[,3]-(exs[,2]+3)+1)/3
+   if (datatype=="SNP")
+      N.nonsy=2*sum(exs[,3]-(exs[,2]+3)+1)/3
+   #option2: assume all positions are called and if missing, it was filtered out
+   if (datatype=="full")
+      N.nonsy=length(ii)
    return(list(sum.theta=th.sum,N=N.nonsy))
 }
 
 #' Calculate Pi_N for loci with a high impact or moderate impact mutation
 #' @export
-Pi_N.par=function(TH, exon.sel, exon.mut,nSNP=NA,cores=15){
+Pi_N.par=function(TH, exon.sel, exon.mut,nSNP=NA,datatype="full",cores=15){
   message("Calculating Pi_N - parallel")
   TH.split=split(TH,TH[,1])
   exon.mut.list=split(exon.mut,exon.mut[,1])
@@ -79,7 +145,7 @@ Pi_N.par=function(TH, exon.sel, exon.mut,nSNP=NA,cores=15){
   TH.split=TH.split[sel.contigs]
   exon.mut.list=exon.mut.list[sel.contigs]
   exon.sel.list=exon.sel.list[sel.contigs]
-  contig.pi=parallel::mcmapply(contig.Pin,TH.split,exon.sel.list,exon.mut.list,mc.cores=min(cores,length(TH.split)))
+  contig.pi=parallel::mcmapply(contig.Pin,TH.split,exon.sel.list,exon.mut.list,datatype,mc.cores=min(cores,length(TH.split)))
   if (is.na(nSNP)){
      res=list(Pi=sum(unlist(contig.pi["sum.theta",]))/sum(unlist(contig.pi["N",])),chrom.sum=contig.pi["sum.theta",],chrom.N=contig.pi["N",])
      message("The number of called nucleotides is not known - now using total exon length")
@@ -110,7 +176,7 @@ Pi_N=function(TH, exon.sel, exon.mut){
      }
    }
   }
-  N.nonsy=sum(exon.sel[,3]-(exon.sel[,2]+3)+1)/3
+  N.nonsy=2*sum(exon.sel[,3]-(exon.sel[,2]+3)+1)/3
   return(th.sum=th.sum,N=N.nonsy)
 }
 
